508b5a973b3acd7b769a95785863fb6294854051
bandidoLando <opikivisto@gmail.com>
2017-03-18 18:23:03 +0200

    route examples added

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,29 @@ use App\Controllers\UserController;
 use App\Controllers\RedirectController;
 
 
-	$app->get('/', function($request, $response) {
+	$app->get('/contact', function($request, $response) {
 
 		return $this->view->render($response, 'home.twig');
 	});
 
+	$app->post('/contact', function($request, $response) {
+
+		// param of your choice **
+		echo $request->getParam('email');
+		
+		// gets all parametres to dump
+		var_dump($request->getParams());
+
+		echo $request->getParsedBodyParam('name');
+		
+
+	})->setName('contact');
+	
+	$app->get('/users', function($request, $response){
+
+		echo $request->getQueruParam('pagenum');
+
+		// returns value
+		// if GET request query includes /users?page=4
+		echo $request->getParam('page');
+	});
