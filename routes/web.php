3b1572339622c81742fb295f6c17cf4a030e7fab
bandidoLando <opikivisto@gmail.com>
2017-03-18 14:05:25 +0200

    working pdo connection added

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,11 @@ use App\Controllers\RedirectController;
 
 	$app->get('/', function($request, $response) {
 
-		return $this->view->render($response, 'home.twig');
+		$users = $this->db->query("SELECT * FROM members")->fetchAll(PDO::FETCH_OBJ);
+	
+		echo '<pre>';
+			var_dump($users);
+		echo '</pre>';
+	
+		// return $this->view->render($response, 'home.twig');
 	});
-
