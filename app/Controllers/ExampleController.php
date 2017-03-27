483d98bccf718e02156596704a293b1448ddd407
bandidoLando <opikivisto@gmail.com>
2017-03-27 12:14:57 +0300

    json_retrive clas created

--- app/Controllers/ExampleController.php
+++ app/Controllers/ExampleController.php
@@ -2,14 +2,17 @@
 
 
 namespace App\Controllers;
-
+use PDO;
 
 class ExampleController extends BaseController {
 
 
-	public function index($request, $response){
+	public function json($request, $response){
 
-		//
+		$topics = $this->c->db->query("SELECT * FROM topics")->fetchAll(PDO::FETCH_OBJ);
+
+		return $response->write(json_encode($topics));
+
 	}
 
 }
